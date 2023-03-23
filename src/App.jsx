f1464b35e270197cf02d559e2537cddee126e8b5
Lazar Stojanović <lakisd98@gmail.com>
2023-03-24 00:43:07 +0100

    Add: page footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import { Fragment } from "react";
 import Card from "./components/UI/Card";
 
 function App() {
 	return (
-		<div className="flex justify-center">
-			<Card />
-		</div>
+		<Fragment>
+			<div className="flex justify-center">
+				<Card />
+			</div>
+			<footer className="fixed w-full bottom-0 text-xs text-lightCyan text-center">Coded by Lazar Stojanović</footer>
+		</Fragment>
 	);
 }
 
